92cc8b80475bd7aeef67c2ebb04bd13004c9186c
uttam kamar <kamaruttam@gmail.com>
2024-09-25 12:39:51 +0530

    impliment the complete layout

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import RightSidebar from "@/components/RightSidebar";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggedIn = {
+    firstName: "Uttam",
+    lastName: "Kamar",
+    email: "kamaruttam@gmail.com",
+  };
   return (
-    <div>
-      This is home page
-    </div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="welcome"
+            user={loggedIn?.firstName || "Guest"}
+            subtext="Access and manage your account and transaction efficiently."
+          />
 
-export default Home
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1340.45}
+          />
+        </header>
+        Recent transction
+      </div>
+
+      <RightSidebar
+        user={loggedIn}
+        transactions={[]}
+        banks={[{ currentBalance: 123.5 }, {currentBalance: 500}]}
+      />
+    </section>
+  );
+};
+
+export default Home;
---
+++ app/(root)/transaction-history/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Transactionhistory = () => {
+  return <div>Transaction</div>;
+};
+
+export default Transactionhistory;
