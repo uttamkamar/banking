1dea2d4ef980a575eee437dbaf52d786ebdb6540
uttam kamar <kamaruttam@gmail.com>
2024-09-27 11:16:50 +0530

    create form and setup appwrite

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,7 +1,12 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignIn = () => {
-  return <div>Signin</div>;
+  return (
+    <div className="flex-center size-full max-sm:px-6">
+      <AuthForm type="sign-in" />
+    </div>
+  );
 };
 
 export default SignIn;
--- app/(auth)/sign-up/page.tsx
+++ app/(auth)/sign-up/page.tsx
@@ -1,7 +1,10 @@
+import AuthForm from "@/components/AuthForm";
 import React from "react";
 
 const SignUp = () => {
-  return <div>SignUp</div>;
+  return <div className="flex-center size-full max-sm:px-6">
+  <AuthForm type="sign-up" />
+</div>;
 };
 
 export default SignUp;
